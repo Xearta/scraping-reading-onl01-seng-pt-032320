833b638bddacf21fe682433b1798b708e0188e5a
Tanner Townsend <rsmcwow@gmail.com>
2020-04-09 03:03:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,8 +5,8 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 #puts doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC.title-oE5vT4")
 
 
-courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0]
 
-courses.each do |course|
-  puts course.text.strip
-end
+# courses.each do |course|
+#   puts course.text.strip
+# end
